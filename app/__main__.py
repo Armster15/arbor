0dab3155f219cb19cadcd0e37f70c555eb7dc86a
Armaan Aggarwal <armaanaggarwal@gmail.com>
2025-10-16 15:33:39 -0400

    download audio file, which doesnt require ffmpeg either!

--- app/__main__.py
+++ app/__main__.py
@@ -9,7 +9,7 @@ import tempfile
 
 # Configuration options matching the command line flags
 ydl_opts = {
-    "format": "best",  # Download the worst quality available
+    "format": "bestaudio[ext=m4a]/bestaudio",
     "nopostoverwrites": True,
     "postprocessors": [],  # No post-processors at all
     "verbose": True,  # Shows detailed output including ffmpeg usage'
