b407f4e1e63a24accf9ec3958c4ae13ba77585ff
Armaan Aggarwal <armaanaggarwal@gmail.com>
2025-10-16 13:33:12 -0400

    clarify citation

--- pytest/main.m
+++ pytest/main.m
@@ -1,7 +1,9 @@
 //
-//  NOT MINE. Source: https://github.com/beeware/briefcase-iOS-Xcode-template/blob/main/%7B%7B%20cookiecutter.format%20%7D%7D/%7B%7B%20cookiecutter.class_name%20%7D%7D/main.m
+//  Original Source: https://github.com/beeware/briefcase-iOS-Xcode-template/blob/main/%7B%7B%20cookiecutter.format%20%7D%7D/%7B%7B%20cookiecutter.class_name%20%7D%7D/main.m
 //  See for more info: https://github.com/beeware/Python-Apple-support/blob/main/USAGE.md
-
+//
+//  This file does have modifications made by me though.
+//
 //  main.m
 //  A main module for starting Python projects under iOS.
 //
