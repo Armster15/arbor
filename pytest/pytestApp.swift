7bd2f3045a0d93010f99b553214fd475b199ac74
Armaan Aggarwal <armaanaggarwal@gmail.com>
2025-10-16 11:14:58 -0400

    Initial Commit

---
+++ pytest/pytestApp.swift
@@ -0,0 +1,17 @@
+//
+//  pytestApp.swift
+//  pytest
+//
+//  Created by Armaan Aggarwal on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct pytestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
