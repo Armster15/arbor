c92d8a8a209abd270f64cdf5c8790bf040a4522f
Armaan Aggarwal <armaanaggarwal@gmail.com>
2025-10-19 10:08:50 -0700

    remove old configureaudiosession code

--- pytest/pytestApp.swift
+++ pytest/pytestApp.swift
@@ -12,21 +12,10 @@ import AVFoundation
 struct pytestApp: App {
     init() {
         _ = start_python_runtime(CommandLine.argc, CommandLine.unsafeArgv)
-            configureAudioSession()
     }
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
-
-        private func configureAudioSession() {
-            do {
-                let session = AVAudioSession.sharedInstance()
-                try session.setCategory(.playback, mode: .default, options: [.allowAirPlay, .duckOthers])
-                try session.setActive(true)
-            } catch {
-                print("[AudioSession] Failed to configure: \(error)")
-            }
-        }
 }
