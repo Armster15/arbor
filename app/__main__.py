46744a9204396ba9c0c4ad9290d89ba8856b8e0e
Armaan Aggarwal <49625265+Armster15@users.noreply.github.com>
2026-01-08 00:52:49 -0800

    Python code cleanup (#55)
    Cleanup Python code setup
    
    1. Rename "pytest_download" to "arbor"
    - To more accurately reflect that this is the directory the Python
    source code of Arbor lives in
    2. Update comment in `app/__main__.py`
    3. Add README to `app_packages` explaining what it is

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,1 @@
-# Your application code should be placed in this directory.
-#
-# The native code will be looking for a {{ cookiecutter.module_name }}/__main__.py file as the entry point.
+# This file is executed on app startup. See main.m for more info.
