a884d4d53329299ebefa28f7669ac853af49fdca
Armaan Aggarwal <armaanaggarwal@gmail.com>
2026-01-08 13:43:15 -0800

    update comment

--- app_packages/arbor/pip_exec.py
+++ app_packages/arbor/pip_exec.py
@@ -1,9 +1,8 @@
-import tempfile
-from pathlib import Path
-
-
 # A simple wrapper that lets us programatically call pip.
 # We have to do this because pip uses sys.exit(code) and we don't want to exit the program.
+#
+# NOTE: this isn't recommended per https://github.com/pypa/pip/issues/7498.
+# But lol!
 def pip_exec(argv: list[str]) -> int:
     try:
         from pip._internal.cli.main import main
