056ca55594b3b37be89f98cb3c160c488eeaecd7
Armaan Aggarwal <49625265+Armster15@users.noreply.github.com>
2026-01-09 21:03:07 -0800

    Add Python method to romanize any arbitrary text (#70)
    This code was mostly written by a human (me!!!)
    
    The reason why I am not committing to main directly is because this adds a new dependency, `googletrans`.

--- app_packages/arbor/__init__.py
+++ app_packages/arbor/__init__.py
@@ -1,7 +1,8 @@
 from .download import download
 from .lyrics import get_lyrics_from_youtube
-from .search import search_youtube, search_soundcloud
 from .pip_exec import pip_exec
+from .search import search_youtube, search_soundcloud
+from .translate import translate
 from .update_pkgs import (
     update_pkgs,
     are_pkgs_updated,
